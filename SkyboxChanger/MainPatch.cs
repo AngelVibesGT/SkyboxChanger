4d41933170c74a204b90f6da9c3faf6c6fe0d161
AngelVibes <emullaney710@gmail.com>
2023-09-28 17:27:15 -0400

    Add project files.

---
+++ SkyboxChanger/MainPatch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using emotitron.Compression;
+
+namespace SkyboxChanger
+{
+    [BepInPlugin(PluginInfo.modGUID, PluginInfo.modName, PluginInfo.modVersion)]
+    public class MainPatch : BaseUnityPlugin
+    {
+        // public void Awake()
+        //{
+        // var harmony = new Harmony(PluginInfo.modGUID);
+        //harmony.PatchAll(Assembly.GetExecutingAssembly());
+        // }
+
+        static Material SkyboxMaterial;
+
+        static GameObject obj = null;
+
+        static bool LoadingImage;
+
+
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+            //Checks if image is loaded and skybox is loaded
+            if (!LoadingImage && GameObject.Find("Environment Objects/LocalObjects_Prefab/Standard Sky") != null)
+            {
+                //sets skybox material
+                SkyboxMaterial = new Material(Shader.Find("GorillaTag/UberShader"));
+                //tells the shader to use the texture
+                SkyboxMaterial.shaderKeywords = new string[]
+                {
+                    "_USE_TEXTURE",
+                };
+                //SkyboxMaterial.color = Color.white;
+                LoadingImage = true;
+                //path to the file
+                string AppPath = Application.dataPath;
+                AppPath = AppPath.Replace("/Gorilla Tag_Data", "");
+                string path = AppPath + @"/BepInEx/plugins/Skybox/";
+                Debug.Log (path);
+                //loads the file as bytes
+                byte[] bytes = File.ReadAllBytes(Directory.GetFiles(path, "*.jpeg")[0]);
+                Texture2D loadTexture = new Texture2D(4096, 4096);
+                ImageConversion.LoadImage(loadTexture, bytes);
+                loadTexture.Apply();
+                //byte[] debugImage = loadTexture.EncodeToPNG();
+                //File.WriteAllBytes(path + "/../Img1.png", debugImage);
+                //applies the texture
+                SkyboxMaterial.mainTexture = loadTexture;
+                //finds the skybox and sets its material to the new one
+                obj = GameObject.Find("Environment Objects/LocalObjects_Prefab/Standard Sky");
+                obj.GetComponent<MeshRenderer>().material = SkyboxMaterial;
+                obj.GetComponentInChildren<MeshRenderer>().material = SkyboxMaterial;
+            }
+        }
+    }
+}
---
+++ SkyboxChanger/PluginInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SkyboxChanger
+{
+    internal class PluginInfo
+    {
+        //GUID
+        public const string modGUID = "Skybox.Changer.AV";
+        //name
+        public const string modName = "Skybox Changer";
+        //version
+        public const string modVersion = "0.0.0.1";
+    }
+}
